e7a7cc278efc6d3d4d4e3299bf75a680c368233f
Ethan Shen <ethanshen.wu@gmail.com>
2020-01-17 16:57:33 -0800

    can search cities and receive weather and temperature conditions

--- src/NewCityDisplay.js
+++ src/NewCityDisplay.js
@@ -3,27 +3,33 @@ import Title from './Title';
 import CityLookupForm from './CityLookupForm';
 
 const NewCityDisplay = () => {
-  const [city, updateCity] = useState('');
+  const [city, updateCity] = useState({
+    name: '',
+    main: null,
+    weather: null
+  });
   const [country, updateCountry] = useState('');
 
-  const updateCityInfo = async ({cityName, countryName}) => {
+  const updateCityInfo = async ({cityName: cityInput, countryName: countryInput}) => {
     try {
-      const countryResp = await fetch(`https://restcountries.eu/rest/v2/name/${countryName}?fields=name;alpha2Code`);
+      const countryResp = await fetch(`https://restcountries.eu/rest/v2/name/${countryInput}?fields=name;alpha2Code`);
       const countryInfo = await countryResp.json();
-      const {alpha2Code, name} = countryInfo[0];
-      const weatherResp = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${cityName},${alpha2Code}&appid=318bc9baa109f7aee9113d2a80134751&units=imperial`)
-      const city = await weatherResp.json();
-      updateCity(city.name)
-      updateCountry(name);
+      const {alpha2Code, name: countryName} = countryInfo[0];
+      const weatherResp = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${cityInput},${alpha2Code}&appid=318bc9baa109f7aee9113d2a80134751&units=imperial`)
+      const {main, weather, name: cityName} = await weatherResp.json();
+      updateCity({main, weather, name: cityName})
+      updateCountry(countryName);
     } catch (err){
-      console.log('error');
+      console.log(err);
     }
   }
 
   return (
     <div>
      <CityLookupForm handleSubmit=               {updateCityInfo}/>
-      <Title city={city} country={country}/>
+      <Title city={city.name && city.name} 
+              country={country} 
+              temp={city.main && city.main.temp} weather={city.weather && city.weather[0].main} />
     </div>
   )
 }
--- src/Title.js
+++ src/Title.js
@@ -1,10 +1,14 @@
 import React from 'react';
 
 const Title = (props) => {
-  const {city, country} = props;
+  const {city, country, temp, weather} = props;
   return (
     city ? 
-    <h1>{city}, {country}</h1> : 
+    <>
+      <h1>{city}, {country}</h1> 
+      <p>Temperature: {temp}</p>
+      <p>Weather: {weather}</p>
+    </>: 
     <h1>Nothing's here yet</h1>
   );
 }
