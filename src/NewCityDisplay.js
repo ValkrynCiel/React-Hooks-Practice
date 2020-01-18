b37b143b6eb7c05a60402258649887dcbf22993f
Ethan Shen <ethanshen.wu@gmail.com>
2020-01-17 17:29:27 -0800

    basic error handling for incorrect city names

--- src/NewCityDisplay.js
+++ src/NewCityDisplay.js
@@ -12,15 +12,36 @@ const NewCityDisplay = () => {
 
   const updateCityInfo = async ({cityName: cityInput, countryName: countryInput}) => {
     try {
-      const countryResp = await fetch(`https://restcountries.eu/rest/v2/name/${countryInput}?fields=name;alpha2Code`);
-      const countryInfo = await countryResp.json();
-      const {alpha2Code, name: countryName} = countryInfo[0];
-      const weatherResp = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${cityInput},${alpha2Code}&appid=318bc9baa109f7aee9113d2a80134751&units=imperial`)
-      const {main, weather, name: cityName} = await weatherResp.json();
-      updateCity({main, weather, name: cityName})
-      updateCountry(countryName);
+      if (countryInput) {
+        const countryResp = await fetch(`https://restcountries.eu/rest/v2/name/${countryInput}?fields=name;alpha2Code`);
+        const countryInfo = await countryResp.json();
+        debugger
+        if (countryResp.status !== 200) {
+          throw Error(`Oops! We couldn't find ${countryInput} on the map!`)
+        }
+        const {alpha2Code, name: countryName} = countryInfo[0];
+        const weatherResp = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${cityInput},${alpha2Code}&appid=318bc9baa109f7aee9113d2a80134751&units=imperial`)
+        if (weatherResp.status !== 200) {
+          throw Error(`Oops! We couldn't find ${cityInput}, ${countryInput} on the map!`)
+        }
+        const {main, weather, name: cityName} = await weatherResp.json();
+        updateCountry(countryName);
+        updateCity({main, weather, name: cityName})
+      } else {
+        const weatherResp = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${cityInput}&appid=318bc9baa109f7aee9113d2a80134751&units=imperial`)
+        debugger
+        if (weatherResp.status !== 200) {
+          throw Error(`Oops! We couldn't find ${cityInput} on the map!`)
+        }
+        const {main, weather, sys, name: cityName} = await weatherResp.json();
+        const countryResp = await fetch(`https://restcountries.eu/rest/v2/alpha/${sys.country}`);
+        const {name: countryName} = await countryResp.json();
+        updateCountry(countryName);
+        updateCity({main, weather, name: cityName});
+
+      }
     } catch (err){
-      console.log(err);
+      console.log(err)
     }
   }
 
@@ -29,7 +50,7 @@ const NewCityDisplay = () => {
      <CityLookupForm handleSubmit=               {updateCityInfo}/>
       <Title city={city.name && city.name} 
               country={country} 
-              temp={city.main && city.main.temp} weather={city.weather && city.weather[0].main} />
+              temp={city.main && city.main.temp} weather={city.weather && city.weather[0].description} />
     </div>
   )
 }
